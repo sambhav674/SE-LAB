f8f2b1a4e42276a8b234941fb8a211950cf87581
Shashank <shashankreddy512512@gmail.com>
2025-12-02 12:42:42 +0530

    Maven Java Project

---
+++ src/main/java/com/example/externallab/App.java
@@ -0,0 +1,10 @@
+package com.example.externallab;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
